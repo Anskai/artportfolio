78026da8749415ebe52dd51639923902b01a5be2
Arina Antskaitis <a.antskaitis@oth-aw.de>
2022-11-15 15:30:03 +0100

    pages added

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,9 @@
 import '../styles/globals.css'
+import About from "../src/components/About";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}) {
+    return <Component {...pageProps} />
+
 }
 
 export default MyApp
